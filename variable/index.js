916634c77a9a42707e2302210f9a4b2ce42a6dfa
Everton Lopes <everton.lopess@outlook.com>
2021-03-08 15:48:21 -0300

    new file: start project

---
+++ variable/index.js
@@ -0,0 +1,47 @@
+console.log("Begin process...");
+var num = 1;
+num2 = 3;
+var price = 1.5;
+var myName = "Arroz";
+var trueValue = true;
+var nullVar = null;
+var und;
+
+//Printing using "+" concatenates the variable with the text
+console.log("num:" + num);
+console.log("num2:" + num2);
+console.log("price:" + price);
+console.log("myName:" + myName);
+console.log("trueValue:" + trueValue);
+console.log("nullVar:" + nullVar);
+console.log("undefined:" + und);
+
+//Printing using "," add a description e view the content of the variable,
+//case it is an object
+console.log("num:", num);
+console.log("num2:", num2);
+console.log("price:", price);
+console.log("myName:", myName);
+console.log("trueValue:", trueValue);
+console.log("nullVar:", nullVar);
+console.log("undefined:", und);
+
+//Printing using "${}" concatenates the variable with the text
+console.log(`num: ${num}`);
+console.log(`num2: ${num2}`);
+console.log(`price: ${price}`);
+console.log(`myName: ${myName}`);
+console.log(`trueValue: ${trueValue}`);
+console.log(`nullVar: ${nullVar}`);
+console.log(`undefined: ${und}`);
+
+//Write in the html
+document.write(num);
+document.write(num2);
+document.write(price);
+document.write(myName);
+document.write(trueValue);
+document.write(nullVar);
+document.write(und);
+
+console.log("... End process");
