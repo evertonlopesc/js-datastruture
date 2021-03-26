898bb1bd1e7b69bc116fa1e7a6275e31b186e9c1
Everton Costa <everton.lopess@outlook.com>
2021-03-26 15:27:15 -0300

    new file: operator exponential and modules

---
+++ operator-exponential/modules/calcArea.js
@@ -0,0 +1,12 @@
+// Modules
+
+//  export function circleArea(r) {
+//    return 3.14 * (r ** 2);
+//  };
+//  
+//  export function squareArea(s) {
+//    return s * s;
+//  };
+
+export const circleArea = r => 3.14 * (r ** 2);
+export const squareArea = s => s * s;
