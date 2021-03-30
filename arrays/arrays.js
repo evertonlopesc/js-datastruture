8467c2f418b7c4b3352bac6b5f3f0c74378d1227
Everton Costa <everton.lopess@outlook.com>
2021-03-30 12:50:03 -0300

    Title: Array Insert and Remove
    Description: pop, push, unshift and functions

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -7,11 +7,10 @@ daysOfWeek = new Array(
 );
 
 for (let index = 0; index < daysOfWeek.length; index++) {
-    console.log(daysOfWeek[index]);  
+    daysOfWeek[index];
 };
-
-console.log();
-
+console.log("DAYS OF WEEK");
+console.log(daysOfWeek);
 let daysOfWeekTwo = [];
 let daysOfWeekThree = [
     "Monday", "Sunday", "Tuesdays", "Wednesday", "Thursday", "Friday", "Saturday"
@@ -30,46 +29,124 @@ for (let index = 3; index < 20; index++) {
 
 };
 for (let index = 1; index < fibonacci.length; index++) {
-    console.log(fibonacci[index]);    
+    fibonacci[index];
 };
+console.log("FIBONACCI");
+console.log(fibonacci);
 
 // Add elements in Array
 let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log("NEW ARRAY NUMBERS");
 console.log(numbers);
 
 numbers[numbers.length] = 10;
- console.log(numbers);
+console.log("INSERT WITH .LENGTH");
+console.log(numbers);
 
 // Push
 numbers.push(11);
- console.log(numbers);
+console.log("INSERT WITH PUSH");
+console.log(numbers);
 
 // Insert first position of the Array
-
 // My method
-// function insertFirstIndex(arrayss, value) {
-//     let newArray = [];
-//     newArray[0] = value;
-//     for (let index = 0; index < arrayss.length; index++) {
-//         newArray[index + 1] = arrayss[index];
-//     }
-//     arrayss = newArray;
-// 
-//     return console.log(arrayss);
-// };
+function insertFirstIndex(array, value) {
+    let arrayAux = [];
+    arrayAux[0] = value;
+    for (let index = 0; index < array.length; index++) {
+        arrayAux[index + 1] = array[index];
+    }
+    return array = arrayAux;
+};
 
-// insertFirstIndex(numbers, (-1));
+numbers = insertFirstIndex(numbers, (-1));
+console.log("INSERT FIRST WITH MY FUNCTION");
+console.log(numbers);
 
 // Method Book Loiane
 Array.prototype.insertFirstPosition = function(value) {
     for (let i = this.length; i >= 0; i--) {
-        this[i] = this[i - 1];   
-        console.log(this[i]);
+        this[i] = this[i - 1]; 
     }
     this[0] = value;
 };
-numbers.insertFirstPosition(-1);
+
+numbers.insertFirstPosition(-2);
+console.log("INSERT FIRST OF THE LOIANE");
 console.log(numbers);
 
-numbers.unshift(-2);
+// Class Unshift insert in the first position of a array
+numbers.unshift(-3);
+console.log("INSERT UNSHIFT");
+console.log(numbers);
+
+numbers.unshift(-5, -4);
+console.log("INSERT UNSHIFT WHIT TWO NUMBERS");
+console.log(numbers);
+
+// Removing element in the last array
+numbers.pop();
+console.log("REMOVE WITH POP");
+console.log(numbers);
+
+// Removing element of the first position of a array
+
+for (let i = 0; i < numbers.length; i++) {
+    numbers[i] = numbers[i + 1];
+}
+console.log("REMOVE ONE POSITION ARRAY");
+console.log(numbers);
+
+// MY METHOD
+function removeGarbageArray(array) {
+    const cleanArray = [];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] !== undefined) {
+            cleanArray[i] = array[i];
+        };
+    };
+    return array = cleanArray;
+};
+
+function removingFirstPosition(array) {
+    const arrayAux = [];
+    for (let i = 0; i < array.length; i++) {
+        arrayAux[i] = array[i + 1];
+    };
+    array = arrayAux
+    return removeGarbageArray(array);
+};
+
+numbers = removingFirstPosition(numbers);
+console.log("REMOVE WHIT MY FUNCTION");
+console.log(numbers);
+
+// METHOD BOOK LOIANE
+for (let i = 0; i < numbers.length; i++) {
+    numbers[i] = numbers[i + 1];
+}
+console.log("REMOVE ONE POSITION ARRAY");
+console.log(numbers);
+
+Array.prototype.reIndex = function(myArray) {
+    const newArray = [];
+    for (let i = 0; i < myArray.length; i++) {
+        if (myArray[i] !== undefined) {
+            newArray.push(myArray[i]);
+        }
+    };
+    return newArray;
+};
+
+// remove the first position manually and execute reIndex
+Array.prototype.removeFirstPosition = function() {
+    for (let i = 0; i < this.length; i++) {
+        this[i] = this[i + 1];
+        
+    };
+    return this.reIndex(this);
+};
+
+numbers = numbers.removeFirstPosition();
+console.log("REMOVE ONE POSITION ARRAY - LOIANE");
 console.log(numbers);
