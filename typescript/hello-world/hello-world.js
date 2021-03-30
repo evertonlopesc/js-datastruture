4b74b2d6c885c0750de57e33da4ad64d4d5b0eb7
Everton Costa <everton.lopess@outlook.com>
2021-03-30 07:11:27 -0300

    Title: Introduction Typescript
    Description: new file hello-world and interfaces creates.

---
+++ typescript/hello-world/hello-world.js
@@ -0,0 +1,20 @@
+// let myName = "Packt";
+// myName = 10; // Error type
+// let age: number = 20;
+// let existsFlag: boolean = true;
+// let language: string = "Javascript";
+var helloWorld = "Hello World!!!";
+console.log(helloWorld);
+var age = 20; // Number
+var existsFlag = true; // Boolean
+var language = "Javascript"; // String
+// It is recommended to assign a type to a variable 
+// when it is not initialized.
+var favoriteLanguage;
+var langs = ["Javascript", "Ruby", "Python"];
+favoriteLanguage = langs[0];
+console.log(age);
+console.log(existsFlag);
+console.log(language);
+console.log(langs);
+console.log(favoriteLanguage);
---
+++ typescript/hello-world/hello-world.ts
@@ -0,0 +1,25 @@
+// let myName = "Packt";
+// myName = 10; // Error type
+
+// let age: number = 20;
+// let existsFlag: boolean = true;
+// let language: string = "Javascript";
+
+let helloWorld: string = "Hello World!!!";
+console.log(helloWorld);
+
+let age = 20; // Number
+let existsFlag = true; // Boolean
+let language = "Javascript"; // String
+
+// It is recommended to assign a type to a variable 
+// when it is not initialized.
+let favoriteLanguage: string;
+let langs = ["Javascript", "Ruby", "Python"];
+favoriteLanguage = langs[0];
+
+console.log(age);
+console.log(existsFlag);
+console.log(language);
+console.log(langs);
+console.log(favoriteLanguage);
