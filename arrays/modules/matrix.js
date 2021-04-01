9e5ed946fa2875077c75fa12edc3f52dab85d015
Everton Costa <everton.lopess@outlook.com>
2021-04-01 06:56:46 -0300

    Title: Modified matrix.js
    Description: used console.table and add print in for.

--- arrays/modules/matrix.js
+++ arrays/modules/matrix.js
@@ -7,8 +7,6 @@ export default function initMatrixTwoDimensional() {
     averangeTemp[0] = [72, 75, 79, 79, 81, 81];
     averangeTemp[1] = [81, 79, 75, 75, 73, 72];
 
-    console.log(averangeTemp);
-
     // Other Day 1
     let averangeTempDay = [];
     averangeTempDay[0] = [];
@@ -21,14 +19,24 @@ export default function initMatrixTwoDimensional() {
 
     // Day 2
     averangeTempDay[1] = [];
-    averangeTempDay[1][0] = 72;
-    averangeTempDay[1][1] = 75;
-    averangeTempDay[1][2] = 79;
-    averangeTempDay[1][3] = 79;
-    averangeTempDay[1][4] = 81;
-    averangeTempDay[1][5] = 81;
+    averangeTempDay[1][0] = 81;
+    averangeTempDay[1][1] = 79;
+    averangeTempDay[1][2] = 75;
+    averangeTempDay[1][3] = 75;
+    averangeTempDay[1][4] = 73;
+    averangeTempDay[1][5] = 73;
 
-    console.log(averangeTempDay);
+    function printMatrix(myMatrix) {
+        for (let l = 0; l < myMatrix.length; l++) {
+            console.log(l + ": ");
+            for (let c = 0; c < myMatrix[l].length; c++) {
+                console.log(c +": " + myMatrix[l][c]);                
+            };
+        };
+    };
+
+    console.table(averangeTempDay);
+    printMatrix(averangeTempDay);
 }
 
 export function initMatrixMultDimensional() {
