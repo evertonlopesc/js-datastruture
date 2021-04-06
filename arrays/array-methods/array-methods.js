fc526e3a65df7f685735da6a334759c0b196aeeb
Everton Costa <everton.lopess@outlook.com>
2021-04-06 07:13:01 -0300

    Title: Modified array-methods.js
    Description: Learning Custom shorting, Sorting strings methods and
    create function printTitle.

--- arrays/array-methods/array-methods.js
+++ arrays/array-methods/array-methods.js
@@ -4,6 +4,10 @@ function printScreen(chars, param) {
     return console.log(`${chars}: ${param}`);
 };
 
+function printTitle(param) {
+    return console.log("-------------" + param + "-------------");
+}
+
 const arrays = [
     "concat", "every", "filter", "forEach", "join", "indexOf", "lastIndexOf", 
     "map","reverse", " slice", "some", "sort", "toString", "valueOf"
@@ -13,7 +17,7 @@ printScreen("Methods", arrays);
 
 // CONCAT
 // It need of three elements, array + junction variable + array
-console.log("Concat Method");
+printTitle("Concat Method");
 const zero = 0;
 const poristiveNumbers = [1, 2, 3];
 const negativeNumbers = [-3, -2, -1];
@@ -38,45 +42,45 @@ let numbersPair = [2,2,2,2,2,2];
 let numbersOdd  = [3,3,3,3,3];
 
 // Every Method - if there is an error in the array, the result is false
-console.log("Every Method");
+printTitle("Every Method");
 console.log(numbersMixed.every(isEven));
 console.log(numbersPair.every(isEven));
 
 // Some method - if there is an error in the array, the result is true
-console.log("Some method");
+printTitle("Some method");
 console.log(numbersMixed.some(isEven));
 console.log(numbersOdd.some(isEven));
 
 // forEach Method - function isEven is not used.
-console.log("forEach Method");
+printTitle("forEach Method");
 numbersMixed.forEach(
     param => console.log(`${param} is pair: ${param % 2 === 0}`)
 );
 
 // Map Method - add the result array to a variable
-console.log("Map Method");
+printTitle("Map Method");
 const myMap = numbersMixed.map(isEven);
 console.log(myMap);
 
 // Filter Method - filter the results array for a variable
-console.log("Filter Method");
+printTitle("Filter Method");
 const evenNumbers = numbersMixed.filter(isEven);
 console.log(evenNumbers);
 
 // Reduce Method - can use conditions as a result of the array to a variable
-console.log("Reduce Method");
+printTitle("Reduce Method");
 console.log(
     numbersMixed.reduce((previous, current) => previous + current)
 );
 
 // for...of
-console.log("for...of");
+printTitle("for...of");
 for (const index of numbersMixed) {
     console.log(`${index} is: ${index % 2 === 0 ? "pair" : "odd"}`);
 };
 
 // Object @@iterator
-console.log("Object @@iterator");
+printTitle("Object @@iterator");
 let iterator = numbersMixed[Symbol.iterator]();
 console.log(iterator.next().value);
 for (const index of iterator) {
@@ -85,7 +89,7 @@ for (const index of iterator) {
 console.log(iterator.next().value);
 
 // Entries Method
-console.log("Entries Method");
+printTitle("Entries Method");
 let aEntries = numbersMixed.entries();
 console.log(aEntries.next().value);
 for (const index of aEntries) {
@@ -94,7 +98,7 @@ for (const index of aEntries) {
 console.log(aEntries.next().value);
 
 // Keys Method
-console.log("Keys Method");
+printTitle("Keys Method");
 let aKeys = numbersMixed.keys();
 console.log(aKeys.next());
 for (const index of aKeys) {
@@ -103,7 +107,7 @@ for (const index of aKeys) {
 console.log(aKeys.next());
 
 // Values Method
-console.log("Values Method");
+printTitle("Values Method");
 const aValues = numbersMixed.values();
 console.log(aValues.next());
 for (const index of aValues) {
@@ -112,7 +116,7 @@ for (const index of aValues) {
 console.log(aValues.next());
 
 // Array.from Method
-console.log("Array.from Method");
+printTitle("Array.from Method");
 let newNumbers = Array.from(numbersMixed);
 console.log(newNumbers);
 
@@ -120,7 +124,7 @@ let evens = Array.from(numbersMixed, index => (index % 2 == 0));
 console.log(evens);
 
 // Array.of Method
-console.log("Array.of Method");
+printTitle("Array.of Method");
 let newArray = Array.of(1);
 console.log(newArray);
 
@@ -131,7 +135,7 @@ let arrayCopie = Array.of(...numbersMixed);
 console.log(arrayCopie);
 
 // Fill Method
-console.log("Fill Method");
+printTitle("Fill Method");
 let numberCopy = Array.of(1,2,3,4,5);
 console.log("numberCopy: " + numberCopy);
 
@@ -148,7 +152,7 @@ let ones = Array(6).fill(1);
 console.log(ones);
 
 // CopyWithin Method
-console.log("CopyWithin Method");
+printTitle("CopyWithin Method");
 let copyArray = [1, 2, 3, 4, 5, 6];
 console.log(copyArray);
 
@@ -166,13 +170,15 @@ console.log(copyArray);
  */
 
 // Reserve Method
-console.log("Reserve Method");
+printTitle("Reserve Method");
 let numberRandom = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 console.log("numberRandom: " + numberRandom);
 
 numberRandom.reverse();
 console.log("numberRandom: " + numberRandom);
 
+// Sort Method
+printTitle("Sort Method");
 numberRandom.sort();
 console.log("numberRandom: " + numberRandom);
 
@@ -181,7 +187,7 @@ console.log("numberRandom: " + numberRandom);
 
 numberRandom = [12, 9, 3, 10, 5, -6, 7, 8, 1, 4, 11, 2];
 console.log("numberRandom: " + numberRandom);
-numberRandom.sort((a, b) => a - b);
+numberRandom.sort((prev, next) => prev - next);
 console.log("numberRandom: " + numberRandom);
 
 function compare(previus, next) {
@@ -197,3 +203,73 @@ numberRandom = [12, 9, 3, 10, 5, -6, 7, 8, 1, 4, 11, 2];
 console.log(numberRandom);
 numberRandom.sort(compare);
 console.log(numberRandom);
+
+// Custom sorting
+printTitle("Custom sorting");
+function printArraysObjection(arrayObjecte) {
+    for (const iterator of arrayObjecte) {
+        console.log(`Name: ${iterator.name}, Age: ${iterator.age}`);
+    };
+};
+
+const friends = [
+    { name: "John", age: 30 },
+    { name: "Sebastian", age: 20 },
+    { name: "Chris", age: 25 }, // comma at the end: ES2017
+];
+// console.log(friends);
+printArraysObjection(friends);
+
+function comparePerson(prev, next) {
+    if (prev.age < next.age) {
+        return -1;
+    } else if (prev.age > next.age) {
+        return 1;
+    } else {
+        return 0;
+    }
+};
+console.log(" ");
+
+friends.sort(comparePerson);
+
+printArraysObjection(friends);
+
+//Sorting Strings
+printTitle("Sorting Strings");
+
+let names = ["Ana", "ana", "john", "John"];
+console.log(names.sort());
+
+// Tabel ASCII ( A=65, J=74, a=97, j=106 )
+console.log(
+    names.sort((prev, next) => {
+        if (prev < next) {
+            return -1;
+        } else if (prev > next) {
+            return 1;
+        } else {
+            return 0;
+        };
+    })
+);
+
+console.log(names.sort((prev, next) => prev.localeCompare(next)));
+
+const names2 = ["Maève", "Maeve", "Maéve"];
+console.log(names2.sort((prev, next) => prev.localeCompare(next)));
+
+friends.sort((prev, next) => prev.name.localeCompare(next.name));
+
+printArraysObjection(friends);
+
+// Search
+printTitle("Search");
+
+
+
+
+
+
+
+
