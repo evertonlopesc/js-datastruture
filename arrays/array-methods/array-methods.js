bee2bdccf8b74478af8030ff615b3f66da5e8dc3
Everton Costa <everton.lopess@outlook.com>
2021-04-07 05:52:06 -0300

    Title: Modified array-methods.js
    Description: Learning Class TypedArray

--- arrays/array-methods/array-methods.js
+++ arrays/array-methods/array-methods.js
@@ -308,5 +308,19 @@ printTitle("toString and Join");
 
 console.log(numbersMixed.toString());
 
-const arrayChanged = numbersMixed.join(" - ");
+const arrayChanged = numbersMixed.join("-");
 printScreen(arrayChanged);
+
+// Class TypedArray
+printTitle("Class TypedArray");
+
+let length = 5;
+let int16 = new Int16Array(length);
+let array16 = [];
+array16.length = length;
+for (let i=0; i<length; i++) {
+    int16[i] = i+1;
+};
+printScreen(int16);
+
+
