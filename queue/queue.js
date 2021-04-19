bf036308048fb6855d6d078ece47304877bcfdbd
Everton Costa <everton.lopess@outlook.com>
2021-04-19 10:22:05 -0300

    Title: Modified queue.js
    Description: Changed class method calls.

--- queue/queue.js
+++ queue/queue.js
@@ -1,19 +1,21 @@
 import Queue from "./modules/queue-class.js";
 
-let queue = new Queue();
+const queue = new Queue();
 
-console.log(queue.isEmpty());
+console.log(`Is empty: ${queue.isEmpty()}`);
+queue.enqueue("John");
+queue.enqueue("Jack");
+queue.enqueue("James");
+queue.enqueue("Alexis");
+console.log(`Is empty: ${queue.isEmpty()}`);
+console.log(queue.toString());
+console.log(`Queue size: ${queue.size()}`);
 queue.dequeue();
-queue.enqueue(20);
-queue.enqueue(22);
-queue.enqueue(26);
-queue.enqueue(31);
-queue.enqueue(39);
-queue.enqueue(54);
-queue.toString();
-queue.dequeue();
-queue.toString();
-queue.size();
-console.log(queue.isEmpty());
-queue.toString();
+console.log(queue.toString());
+console.log(`Queue size: ${queue.size()}`);
+console.log(`Is empty: ${queue.isEmpty()}`);
+console.log(queue.toString());
 queue.peek();
+queue.dequeue();
+console.log(queue.toString());
+console.log(`Queue size: ${queue.size()}`);
